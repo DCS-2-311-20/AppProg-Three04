c8f11392a33fd054bf0e603908ec73c5504418ba
Takashi Minohara <minohara@cs.takushoku-u.ac.jp>
2020-10-18 19:50:53 +0900

    start codes

---
+++ three0401.js
@@ -0,0 +1,82 @@
+//
+// 応用プログラミング 課題6.1
+// $Id$
+//
+"use strict"; // 厳格モード
+
+// ３Ｄページ作成関数の定義
+function init() {
+  const controls = {
+    fov: 50, // 視野角
+    hight: 3,
+    rotation: 2
+  };
+
+  // シーン作成
+  const scene = new THREE.Scene();
+
+  // カメラの設定
+  const camera = new THREE.PerspectiveCamera(
+    controls.fov, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+  // レンダラの設定
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor( 0x406080 );
+  renderer.shadowMap.enabled = true;
+  document.getElementById("WebGL-output")
+    .appendChild(renderer.domElement);
+
+  // テクスチャの読み込み
+
+  // 立方体の作成
+  const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+  const cubeMaterial = new THREE.MeshNormalMaterial();
+  // マテリアルにテクスチャを登録
+  
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.position.y = 2;
+  scene.add(cube);
+
+  // 球の作成
+
+  // 平面の作成
+  const circle = new THREE.Mesh(
+    new THREE.CircleGeometry(20,24),
+    new THREE.MeshBasicMaterial({color:0x006010}));
+  circle.rotation.x = -Math.PI/2;
+  circle.receiveShadow = true;
+  scene.add(circle);
+
+  // 光源の作成
+
+  // 座標軸の設定
+  const axes = new THREE.AxesHelper(3);
+  scene.add(axes);
+
+  // GUIコントローラ
+  const gui = new dat.GUI();
+  // カメラのコントローラ
+  gui.add(controls, "fov", 10, 100);
+  gui.add(controls, "hight", -10, 10);
+  gui.add(controls, "rotation", -10, 10);
+
+  // 描画更新関数の定義
+  function update() {
+    // カメラの設定
+    camera.fov = controls.fov;
+    camera.position.set(7, controls.hight, 5);
+    camera.lookAt(0, 1, 0);
+    camera.updateProjectionMatrix();
+    renderer.render(scene, camera);
+    // 物体の回転
+    cube.rotation.y += 0.01*controls.rotation;
+    // 次のフレーム描画時の呼び出しを要求
+    requestAnimationFrame(update);
+  }
+
+  // 描画
+  update();
+}
+
+document.onload = init();
---
+++ three0402.js
@@ -0,0 +1,130 @@
+//
+// 応用プログラミング 課題6.1
+// $Id$
+//
+"use strict"; // 厳格モード
+
+// ３Ｄページ作成関数の定義
+function init() {
+  const controls = {
+    fov: 60, // 視野角
+    x: 3,
+    y: 2,
+    z: 1,
+    rotation: 1
+  };
+
+  // シーン作成
+  const scene = new THREE.Scene();
+
+  // カメラの設定
+  const camera = new THREE.PerspectiveCamera(
+    controls.fov, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+  // レンダラの設定
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x204060);
+  document.getElementById("WebGL-output")
+    .appendChild(renderer.domElement);
+
+  // テクスチャの読み込み
+  /* 自分で考えよう */
+
+  // 正20面体の作成
+  const geometry = new THREE.IcosahedronGeometry();
+  const material = new THREE.MeshNormalMaterial();
+  // マテリアルにテクスチャを登録
+  /* 自分で考えよう */
+
+  // UVマッピング
+  //  基準座標と間隔
+  const x0 = 0.0125;
+  const y0 = 1.0;
+  const dx = 0.089;
+  const dy = 0.219;
+  //   UVマッピングのための関数
+  function setUvs(f, x, y) {
+    switch (y) {
+    case 0: /* 自分で考えよう */
+      geometry.faceVertexUvs[0][f][0].x = x0;
+      geometry.faceVertexUvs[0][f][0].y = y0;
+      geometry.faceVertexUvs[0][f][1].x = x0;
+      geometry.faceVertexUvs[0][f][1].y = y0;
+      geometry.faceVertexUvs[0][f][2].x = x0;
+      geometry.faceVertexUvs[0][f][2].y = y0;
+      break;
+    case 1: /* 自分で考えよう */
+      geometry.faceVertexUvs[0][f][0].x = x0;
+      geometry.faceVertexUvs[0][f][0].y = y0;
+      geometry.faceVertexUvs[0][f][1].x = x0;
+      geometry.faceVertexUvs[0][f][1].y = y0;
+      geometry.faceVertexUvs[0][f][2].x = x0;
+      geometry.faceVertexUvs[0][f][2].y = y0;
+      break;
+    case 2:
+      geometry.faceVertexUvs[0][f][0].x = x0 + dx * (x + 2);
+      geometry.faceVertexUvs[0][f][0].y = y0 - dy * 2;
+      geometry.faceVertexUvs[0][f][1].x = x0 + dx * (x + 1);
+      geometry.faceVertexUvs[0][f][1].y = y0 - dy;
+      geometry.faceVertexUvs[0][f][2].x = x0 + dx * x;
+      geometry.faceVertexUvs[0][f][2].y = y0 - dy * 2;
+      break;
+    case 3:
+      geometry.faceVertexUvs[0][f][0].x = x0 + dx * (x + 2);
+      geometry.faceVertexUvs[0][f][0].y = y0 - dy * 2;
+      geometry.faceVertexUvs[0][f][1].x = x0 + dx * x;
+      geometry.faceVertexUvs[0][f][1].y = y0 - dy * 2;
+      geometry.faceVertexUvs[0][f][2].x = x0 + dx * (x + 1);
+      geometry.faceVertexUvs[0][f][2].y = y0 - dy * 3;
+      break;
+    }
+  }
+  setUvs( 0, 1, 0); setUvs( 1, 3, 0); setUvs( 2, 5, 0); setUvs( 3, 7, 0); setUvs( 4, 9, 0);
+  setUvs( 6, 1, 1); setUvs( 5, 3, 1); setUvs( 9, 5, 1); setUvs( 8, 7, 1); setUvs( 7, 9, 1);
+  setUvs(16, 0, 2); setUvs(15, 2, 2); setUvs(19, 4, 2); setUvs(18, 6, 2); setUvs(17, 8, 2);
+  setUvs(11, 0, 3); setUvs(10, 2, 3); setUvs(14, 4, 3); setUvs(13, 6, 3); setUvs(12, 8, 3);
+  const icosahedron = new THREE.Mesh(geometry, material);
+  scene.add(icosahedron);
+
+  // 光源の作成
+  const ambientLitght = new THREE.AmbientLight();
+  ambientLitght.intensity = 0.3;
+  scene.add(ambientLitght);
+  /* ここに DirectionalLight を追加する */
+
+  // 座標軸の設定
+  const axes = new THREE.AxesHelper(3);
+  scene.add(axes);
+  axes.visible = false;
+
+  // カメラの設定
+  function cameraUpdate() {
+    camera.fov = controls.fov;
+    camera.position.set(controls.x, controls.y, controls.z);
+    camera.lookAt(0, 0, 0);
+    camera.updateProjectionMatrix();
+  }
+
+  // GUIコントローラ
+  const gui = new dat.GUI();
+  // カメラのコントローラ
+  gui.add(controls, "fov", 10, 100).onChange(cameraUpdate);
+  gui.add(controls, "x", -10, 10).onChange(cameraUpdate);
+  gui.add(controls, "y", -10, 10).onChange(cameraUpdate);
+  gui.add(controls, "z", -10, 10).onChange(cameraUpdate);
+  gui.add(controls, "rotation", -10, 10);
+
+  // 描画更新関数の定義
+  function update() {
+    renderer.render(scene, camera);
+    icosahedron.rotation.y += 0.005 * controls.rotation;
+    requestAnimationFrame(update);
+  }
+
+  // 描画
+  cameraUpdate();
+  update();
+}
+
+document.onload = init();
